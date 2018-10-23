073902a03ab39b6f0bfd3b3cd98f81e33c3b11be
kevin bell <kbell0965@email.com>
2018-10-23 22:09:25 +0800

    add headers to api endpoint

--- src/store/saga/api.js
+++ src/store/saga/api.js
@@ -1,7 +1,11 @@
 import * as axios from "axios"
 
 export function sendLoadRequest() {
-    return axios.get("http://192.168.3.239:808/members/blue")
+    return axios.get("http://192.168.3.239:808/members/blue", {
+    	headers: {
+    		'Token': 'abc'
+    	}
+    })
             .then(res => res.data)
             .catch( err => {throw err})
 }
